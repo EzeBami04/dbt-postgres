8563f52bf61bd93c5dd84cf73369752f811d9f3e
Ezekiel Sanmabo <eomoobamidele@gmail.com>
2026-09-23 17:40:31 +0100

    add macros

--- manufact/models/ods/fact_production.sql
+++ manufact/models/ods/fact_production.sql
@@ -1,10 +1,14 @@
-{{config(materialized='incremental', unique_key='production_id', 
-        incremental_strategy='merge', cluster_by = 'production_id', 
-        partition_by={'field': 'production_date', 'data_type': 'date'})
-        }}
-select *
-from {{ref("stg_production")}}
+{{ config(
+    materialized='incremental',
+    unique_key='production_id',
+    incremental_strategy='merge',
+    cluster_by='production_id',
+    partition_by={'field': 'production_date', 'data_type': 'date'}
+) }}
 
-{%if is_incremental()%}
-    where production_date >= (select max(production_date) - interval'1 day' from {{ this }})
-{%endif%}
+select *
+from {{ ref('stg_production') }}
+
+{% if is_incremental() %}
+    where production_date >= (select max(production_date) - interval '1 day' from {{ this }})
+{% endif %}
--- manufact/models/staging/stg_production.sql
+++ manufact/models/staging/stg_production.sql
@@ -1,9 +1,9 @@
 {{config(materialized='view')}}
-with production as (
+with stg_production as (
     select cast(production_id as varchar(50)) as production_id,
         cast(production_date as Date) as production_date,
         cast(product_id as varchar(10)) as product_id, 
-        cast(batch_id as varchar(5)) as batch_id, 
+        cast(batch_id as varchar(5)) as batch_id,
         cast(machine_id as varchar(45)) as machine_id,
         cast(operator_id as VARCHAR(9)) as operator_id,
         cast(planned_quantity as int) planned_qty,
@@ -11,7 +11,7 @@ with production as (
         cast(good_quantity as bigint) as good_qty,
         cast(defective_quantity as int) as defective_qty,
         cast(downtime_minutes as numeric(10, 2)) as downtime_min,
-        cast(production_time_minutes as numeric(10, 2)) as production_time_min,
+        cast(production_time_minutes as numeric(12, 2)) as production_time_min,
         cast(material_cost as numeric(10, 2)) as material_cost, 
         cast(labor_cost as numeric(10, 2)) as labor_cost,
         cast(energy_cost as numeric(10, 2)) as energy_cost,
@@ -19,6 +19,19 @@ with production as (
         cast(shift as varchar(10)) as shift
     from {{ source('staging', 'production') }}
     WHERE production_date BETWEEN '2025-01-01' AND '2026-08-31'
-    )
+    ),
+    material as(
+        select cast(material_id as varchar(10)) as material_id,
+        cast(production_id as varchar(50)) as production_id
+        from {{ source('staging', 'material_usage') }}
+    ), 
+    production as (
+    select a.production_id, a.production_date, a.product_id, a.batch_id, a.machine_id, a.operator_id,
+        a.planned_qty, a.qty_produced, a.good_qty, a.defective_qty, a.downtime_min,
+        a.production_time_min, a.material_cost, a.labor_cost, a.energy_cost,
+        a.maintenance_required, a.shift, b.material_id
+    from stg_production a
+    left join material b on a.production_id = b.production_id
+)
 select *
 from production
