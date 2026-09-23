df8fe48ed0795ef0fa2d0ee9be82c6e59284af2e
Ezekiel Sanmabo <eomoobamidele@gmail.com>
2026-09-23 15:25:52 +0100

    update fact production

--- manufact/models/ods/fact_production.sql
+++ manufact/models/ods/fact_production.sql
@@ -1,10 +1,10 @@
-{{config(materialized='incremental', unique_id='production_id', 
-        incremental_strategy='upsert', cluster_by = 'production_id', 
+{{config(materialized='incremental', unique_key='production_id', 
+        incremental_strategy='merge', cluster_by = 'production_id', 
         partition_by={'field': 'production_date', 'data_type': 'date'})
         }}
 select *
 from {{ref("stg_production")}}
 
 {%if is_incremental()%}
-    where production_date >= (select max(production_date) from {{ this }})
+    where production_date >= (select max(production_date) - interval'1 day' from {{ this }})
 {%endif%}
